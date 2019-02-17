b7acb7aa03371b4bc2cf93a86f8a9e57377dbef0
Albert He <wuhan20110529@sina.com>
2019-02-17 22:37:03 +0800

    路径微调

--- src/main/java/com/learning/search/controller/SearchController.java
+++ src/main/java/com/learning/search/controller/SearchController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/search")
+@RequestMapping("api/searchController")
 @EnableAutoConfiguration
 @RestController
 public class SearchController {
